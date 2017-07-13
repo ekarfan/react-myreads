7930724d6d43a821f66a5a801188a706c838a249
KF3527 <kf3527@att.com>
2017-07-13 13:55:22 -0500

    Finished the code

--- reactnd-project-myreads/src/App.js
+++ reactnd-project-myreads/src/App.js
@@ -7,35 +7,83 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends Component {
+
   state = {
     books: []
   }
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
+      //console.log(books)
       this.setState({ books })
     })
   }
-  searchBook(book) {
-    BooksAPI.search(book).then(book => {
+
+  getAllBooks() {
+    BooksAPI.getAll().then((books) => {
+      //console.log(books)
+      this.setState({ books })
+    })
+  }
+
+  moveBook=(book,shelf)=>{
+    //console.log(book)
+    //console.log(shelf)
+    this.setState({shelf: shelf})
+    if(shelf){
+        BooksAPI.update(book, shelf).then (()=> {
+          //console.log(books)
+          BooksAPI.getAll().then((books) => {
+            console.log(books)
+            this.setState({ books })
+          })
+        }).catch(function(e){
+          console.log('error',e)
+        })
+    }
+
+  }
+
+  createBook(bookid) {
+    BooksAPI.get(bookid).then(book => {
       this.setState(state => ({
         books: state.books.concat([ book ])
       }))
     })
   }
+
+  searchBook = (query) => {
+    this.setState({ query: query.trim() })
+    BooksAPI.search(query, 20).then((books) => {
+      console.log(books)
+      this.setState({ books: books })
+    }).catch(function(e){
+            console.log('error',e)
+          })
+  }
+
+
   render() {
+    const{books}=this.state
     return (
       <div className="app">
-        <Route path='/search' render={({ history }) => (
-          <SearchBooks
-            onSearchBooks={(book) => {
-              this.searchBook(book)
-              history.push('/')
-            }}
-          />
+        <Route exact path='/search' render={({ history }) => (
+            <SearchBooks
+            books={books}
+            shelf={books.shelf}
+            onSearchBook={this.searchBook}
+            onMoveBook={(book,shelf)=>{
+                    this.moveBook(book,shelf)
+                    history.push('/')
+                  }}/>
         )}/>
-        <Route path='/' render={() => (
+        <Route exact path='/' render={() => (
             <ListBooks
-            />
+            books={books}
+            shelf={books.shelf}
+            onMoveBook={(book,shelf)=>{
+                    this.moveBook(book,shelf)}}/>
+
         )}/>
       </div>
     )
--- reactnd-project-myreads/src/SearchBooks.js
+++ reactnd-project-myreads/src/SearchBooks.js
@@ -1,53 +1,86 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 
+
 class SearchBooks extends Component {
+
+  static prpTypes={
+    books: PropTypes.array.isRequired
+  }
+
   state = {
     query: ''
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
+  updateQuery =(query)=>{
+    this.setState({query})
+    this.props.onSearchBook(query)
   }
+
 
   clearQuery = () => {
     this.setState({ query: '' })
   }
 
+
+
+
   render() {
-    const { books } = this.props
+    const{ books, onMoveBook }=this.props
     const { query } = this.state
+
     let showingBooks
     if (query) {
-      const match = new RegExp(escapeRegExp(this.state.query), 'i')
-      showingBooks = books.filter((book) => match.test(book))
+      const match = new RegExp(escapeRegExp(query),'i')
+      showingBooks = books.filter((book) => match.test(book.title) || match.test(book.authors))
     } else {
-      showingBooks = books
+      showingBooks = []
     }
-
+    console.log(showingBooks)
     return (
       <div className="search-books">
         <div className="search-books-bar">
+          <Link
+            to='/'
+            className='close-search'
+            >close
+          </Link>
+          <div className="search-books-input-wrapper">
             <input
-              className='search-books-input-wrapper'
               type="text"
               placeholder="Search by title or author"
-              value={this.state.query}
+              value={query}
               onChange={(event) => this.updateQuery(event.target.value)}
             />
-            <Link
-              to='/search'
-               className='search-book'
-            >Search Book</Link>
+          </div>
         </div>
+
         <div className="search-books-results">
           <ol className="books-grid">
-            {showingBooks.map((book) => (
-              <li key={book.id} className='book-list-item'>
 
-                <div className='contact-details'>
-                  <p>{book.author}</p>
+            {showingBooks.length !== 0 && showingBooks.map((book, i) => (
+              <li  key={book.id} className='book-list-item'>
+                <div className="book">
+                  <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193,
+                      backgroundImage: `url(${book.imageLinks.thumbnail})`
+                      }}>
+                    </div>
+                    <div className="book-shelf-changer">
+                      <select value={book.shelf}
+                        onChange={event=>onMoveBook(book,event.target.value)}>
+                        <option value="none" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="book-title">{ book.title }</div>
+                  <div className="book-authors">{ book.authors }</div>
                 </div>
               </li>
             ))}
